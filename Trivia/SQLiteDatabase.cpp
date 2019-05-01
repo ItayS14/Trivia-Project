6ba340a3a5739aa087af679206ac24e3a2faecfd
Jonathan David <jday.david.2002@gmail.com>
2019-05-01 17:59:33 +0300

    Updated functions to use the new, "executeSQL" function.
    Also, updated the creation function to be better looking.

--- Trivia/SQLiteDatabase.cpp
+++ Trivia/SQLiteDatabase.cpp
@@ -7,12 +7,12 @@
 //SQLite database constructor. Creates a new database if it doesn't exist already.
 SQLiteDatabase::SQLiteDatabase()
 {
-	bool exists = doesFileExist(NAME);
-	int res = sqlite3_open(NAME, &_db);
-	if (res != SQLITE_OK)
-		throw std::exception("Failed to open database!");
-	if (!exists) //check if database already exists, if not create a new one
+	int res = sqlite3_open_v2(NAME, &_db, SQLITE_OPEN_READWRITE, NULL); //check if database already exists, if not create a new one
+	if (res == SQLITE_CANTOPEN)
 		initNewDatabase();
+	else if (res != SQLITE_OK)
+		throw std::exception("Failed to open database!");
+
 }
 
 //A log-in function, checks if there's a user with the specified username.
@@ -20,9 +20,7 @@ bool SQLiteDatabase::doesUserExist(const std::string & username)
 {
 	std::string command = "SELECT * FROM users WHERE username = \"" + username + "\"";
 	bool b = false;
-	int res = sqlite3_exec(_db, command.c_str(), userExistsCallback, &b, nullptr);
-	if (res != SQLITE_OK)
-		throw std::exception(sqlite3_errmsg(_db));
+	executeSQL(command.c_str(), objectExistsCallBack, &b);
 	return b;
 }
 
@@ -30,43 +28,35 @@ bool SQLiteDatabase::doesUserExist(const std::string & username)
 void SQLiteDatabase::addUser(const std::string & username, const std::string & password, const std::string & email)
 {
 	std::string command = "INSERT INTO users VALUES (\"" + username + "\", \"" + password + "\", \"" + email + "\");";
-	int res = sqlite3_exec(_db, command.c_str(), nullptr, nullptr, nullptr);
-	if (res != SQLITE_OK)
-		throw std::exception("Failed to sign up user!");
+	executeSQL(command);
 }
 
 bool SQLiteDatabase::isCorrectPassword(const std::string & username, const std::string & password)
 {
 	std::string command = "SELECT * FROM users WHERE username = \"" + username + "\" AND password = \"" + password + "\"";
-	std::cout << command << std::endl;
 	bool b = false;
-	int res = sqlite3_exec(_db, command.c_str(), userExistsCallback, &b, nullptr);
-	if (res != SQLITE_OK)
-		throw std::exception(sqlite3_errmsg(_db));
+	executeSQL(command, objectExistsCallBack, &b);
 	return b;
-}
-
-//Simple function to check if file with the specified name exists
-bool SQLiteDatabase::doesFileExist(const std::string& name)
-{
-	std::ifstream file(name.c_str());
-	return file.good();
 }
 
 //Create the tables and check that the database was created successfully
 void SQLiteDatabase::initNewDatabase()
 {
-	int res1, res2, res3;
-	res1= sqlite3_exec(_db, "CREATE TABLE \"Users\" ( 'username' TEXT NOT NULL, 'password' TEXT NOT NULL, 'email' TEXT NOT NULL, PRIMARY KEY('username') )", nullptr, nullptr, nullptr);
-	res2 = sqlite3_exec(_db, "CREATE TABLE \"Questions\" ( 'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'question' TEXT NOT NULL, 'correct_ans' TEXT NOT NULL, 'ans2' TEXT NOT NULL, 'ans3' TEXT NOT NULL, 'ans4' TEXT NOT NULL )", nullptr, nullptr, nullptr);
-	res3 = sqlite3_exec(_db, "CREATE TABLE \"Games\" ( 'game_id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'status' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL )", nullptr, nullptr, nullptr);
-	if (res1 != SQLITE_OK || res2 != SQLITE_OK || res3 != SQLITE_OK)
-		throw std::exception("Failed to create database!");
+	sqlite3_open(NAME, &_db);
+	executeSQL("CREATE TABLE \"Users\" ( 'username' TEXT NOT NULL, 'password' TEXT NOT NULL, 'email' TEXT NOT NULL, PRIMARY KEY('username') )");
+	executeSQL("CREATE TABLE \"Questions\" ( 'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'question' TEXT NOT NULL, 'correct_ans' TEXT NOT NULL, 'ans2' TEXT NOT NULL, 'ans3' TEXT NOT NULL, 'ans4' TEXT NOT NULL )");
+	executeSQL("CREATE TABLE \"Games\" ( 'game_id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'status' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL )");
 }
 
 //If this callback is called, it means sqlite have found a user with the specified username
-int SQLiteDatabase::userExistsCallback(void* data, int argc, char** argv, char** azColName)
+int SQLiteDatabase::objectExistsCallBack(void* data, int argc, char** argv, char** azColName)
 {
 	*(bool*)data = true;
 	return 0;
+}
+
+void SQLiteDatabase::executeSQL(const std::string& sql_code, int(handler)(void*, int, char**, char**), void* argument_for_handler)
+{
+	if (sqlite3_exec(_db, sql_code.c_str(), handler, argument_for_handler, nullptr) != SQLITE_OK)
+		throw std::string("Error while connecting to database!");
 }
--- Trivia/SQLiteDatabase.h
+++ Trivia/SQLiteDatabase.h
@@ -5,18 +5,17 @@
 class SQLiteDatabase : public IDatabase
 {
 public:
-	 SQLiteDatabase();
-	 ~SQLiteDatabase() { sqlite3_close(_db); }
-	
+	SQLiteDatabase();
+	~SQLiteDatabase() { sqlite3_close(_db); }
+
 	virtual bool doesUserExist(const std::string& username);
 	virtual void addUser(const std::string& username, const std::string& password, const std::string& email);
 	virtual bool isCorrectPassword(const std::string& username, const std::string& password);
-	
+
 private:
 	sqlite3* _db;
-	
-	bool doesFileExist(const std::string& name);
+
 	void initNewDatabase();
-	
-	static int userExistsCallback(void* data, int argc, char** argv, char** azColName);
+	void executeSQL(const std::string& sql_code, int(handler)(void*, int, char**, char**) = nullptr, void* argument_for_handler = nullptr);
+	static int objectExistsCallBack(void* data, int argc, char** argv, char** azColName);
 };
