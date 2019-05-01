71c422638a8077373ea9da007ad6b286e4d795cb
Jonathan David <jday.david.2002@gmail.com>
2019-05-01 19:17:19 +0300

    Minor changes
    Fixed doesUserExist function and changed default parameter.

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -8,6 +8,6 @@ public:
 	virtual ~IDatabase() = default;
 	virtual bool doesUserExist(const std::string& username) = 0;
 	virtual void addUser(const std::string& username, const std::string& password, const std::string& email) = 0;
-	virtual bool isCorrectPassword(const std::string& username, const std::string& password) = 0;
+	//virtual bool isCorrectPassword(const std::string& username, const std::string& password) = 0;
 	
 };
