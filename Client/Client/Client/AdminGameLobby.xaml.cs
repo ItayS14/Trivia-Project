da581e007191e1f5745fe820d9e5cf5b276d8dcb
Jonathan David <jday.david.2002@gmail.com>
2019-06-19 20:50:31 +0300

    Added new windows - game lobby (for admin and member)

---
+++ Client/Client/Client/AdminGameLobby.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for AdminGameLobby.xaml
+    /// </summary>
+    public partial class AdminGameLobby : Page
+    {
+        private SocketHandler socket;
+        private Room room;
+        public AdminGameLobby(SocketHandler socket, Room room)
+        {
+            InitializeComponent();
+            this.socket = socket;
+            this.room = room;
+        }
+
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                socket.StartGame(room.ID);
+            }
+            catch(Exception excep)
+            {
+                Utlis.ShowErrorMessage(excep.Message);
+            }
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                socket.LeaveRoom(room.ID);
+            }
+            catch (Exception excep)
+            {
+                Utlis.ShowErrorMessage(excep.Message);
+            }
+        }
+    }
+}
---
+++ Client/Client/Client/GameLobby.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for GameLobby.xaml
+    /// </summary>
+    public partial class GameLobby : Page
+    {
+        private SocketHandler socket;
+        private Room room;
+        public GameLobby(SocketHandler socket, Room room)
+        {
+            InitializeComponent();
+            this.socket = socket;
+            this.room = room;
+        }
+        
+        private void Leave_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                socket.LeaveRoom(room.ID);
+            }
+            catch(Exception excep)
+            {
+                Utlis.ShowErrorMessage(excep.Message);
+            }
+        }
+    }
+}
