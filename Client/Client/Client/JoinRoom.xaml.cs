890567699dad4addebd414f8150db355dee50de5
itay shirizly <it.shirizly@gmail.com>
2019-06-25 01:32:24 +0300

    Changed the updateRoomData to be async

--- Client/Client/Client/JoinRoom.xaml.cs
+++ Client/Client/Client/JoinRoom.xaml.cs
@@ -22,13 +22,14 @@ namespace Client
     {
         private SocketHandler socket;
         private Room room;
-
+        private bool finished;
 
         public JoinRoom(SocketHandler socket, Room room) // Should i also display other stuff that belongs to the room?
         {
             InitializeComponent();
             this.socket = socket;
             this.room = room;
+            finished = false;
             try
             {
                 UpdateRoomData();
@@ -45,6 +46,7 @@ namespace Client
             try
             {
                 socket.JoinRoom(room.ID);
+                finished = true; // here so the async function will only stop if there were no errors
                 NavigationService.Navigate(new GameLobby(socket, room,false));
             }
             catch (Exception excep)
@@ -55,6 +57,7 @@ namespace Client
 
         private void BackButton(object sender, RoutedEventArgs e)
         {
+            finished = true;
             NavigationService.Navigate(new RoomsMenu(socket));
         }
 
@@ -63,6 +66,7 @@ namespace Client
             try
             {
                 socket.SignOut();
+                finished = true; // here so the async function will only stop if there were no errors
                 NavigationService.Navigate(new SignIn(socket));
             }
             catch (Exception excep)
@@ -70,24 +74,28 @@ namespace Client
                 Utlis.ShowErrorMessage(excep.Message);
             }
         }
-        private void UpdateRoomData()
-        {
-            
-            Dictionary<string, object> data = socket.GetRoomState(room.ID);
-            
-            List<string> players = Utlis.ObjectToList<string>(data["players"]);
-            // Disable the button if the room is full or running
-            if (Convert.ToInt32(data["state"]) != (int)State.Joinable || players.Count == Convert.ToInt32(data["max_players"]))
-                JoinRoomButton.IsEnabled = false;
-            else
-                JoinRoomButton.IsEnabled = true;
-            AdminTextBox.Text = players[0];
-            foreach (string player in players.Skip(1))
-                Players.Items.Add(player);
-            int type = Convert.ToInt32(data["type"]);
-            RoomTypeText.Text = Enum.GetName(typeof(Types), type).Replace('_', ' ');
-            QuestionsNumberText.Text = Convert.ToString(room.QuestionCount);
-            QuestionTimeText.Text = Convert.ToString(room.TimePerQuestion);
+        private async void UpdateRoomData() // will run once per 5 seconds
+        {        
+            while (!finished)
+            {
+                Dictionary<string, object> data = socket.GetRoomState(room.ID);
+
+                List<string> players = Utlis.ObjectToList<string>(data["players"]);
+                // Disable the button if the room is full or running
+                if (Convert.ToInt32(data["state"]) != (int)State.Joinable || players.Count == Convert.ToInt32(data["max_players"]))
+                    JoinRoomButton.IsEnabled = false;
+                else
+                    JoinRoomButton.IsEnabled = true;
+                AdminTextBox.Text = players[0];
+                foreach (string player in players.Skip(1))
+                    Players.Items.Add(player);
+                int type = Convert.ToInt32(data["type"]);
+                RoomTypeText.Text = Enum.GetName(typeof(Types), type).Replace('_', ' ');
+                QuestionsNumberText.Text = Convert.ToString(room.QuestionCount);
+                QuestionTimeText.Text = Convert.ToString(room.TimePerQuestion);
+
+                await Task.Delay(5000);
+            }
         }
     }
 }
