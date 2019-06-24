2e12d2b13a07a5c2b6cd2c69e0f61a043ff4c7f6
itay shirizly <it.shirizly@gmail.com>
2019-06-24 21:54:29 +0300

    Fixed question struct to get a vector of answers instead of 3 individual answers/

--- Trivia/Question.h
+++ Trivia/Question.h
@@ -4,7 +4,7 @@
 
 struct Question
 {
-	Question(const std::string& question, const std::string& correct_ans, const std::string& ans2, const std::string& ans3, const std::string& ans4);
+	Question(const std::string & question, const std::vector<std::string>& answers, const int correct_ans) : _question(question), _answers(answers), _correct_ans(correct_ans) {}
 
 	std::string _question;
 	std::vector<std::string> _answers;
